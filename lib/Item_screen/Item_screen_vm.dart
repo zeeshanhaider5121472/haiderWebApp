f4f9230d3f6d3dc95e37d2808306e269dabbf5f9
zeeshanhaider5121472 <shani721287@gmail.com>
2024-03-16 01:51:31 +0500

    item_screen_added

--- lib/Item_screen/Item_screen_vm.dart
+++ lib/Item_screen/Item_screen_vm.dart
@@ -1,13 +1,12 @@
-// // ignore_for_file: file_names
 
-// import 'package:stacked/stacked.dart';
+import 'package:stacked/stacked.dart';
 
-// class ItemScreenVM extends BaseViewModel {
-//   final testList = [
-//     "Compressor runs but produces no compressed air.",
-//     "Cooling oil runs out of the intake filter.",
-//     "Compressor switches between LOAD and IDLE more than twice per minute.",
-//     "Cooling oil leaking into the machine interior.",
-//     "Cooling oil consumption too high.",
-//   ];
-// }
+class ItemScreenVM extends BaseViewModel {
+  final testList = [
+    "Compressor runs but produces no compressed air.",
+    "Cooling oil runs out of the intake filter.",
+    "Compressor switches between LOAD and IDLE more than twice per minute.",
+    "Cooling oil leaking into the machine interior.",
+    "Cooling oil consumption too high.",
+  ];
+}
--- lib/Item_screen/Item_screen_vu.dart
+++ lib/Item_screen/Item_screen_vu.dart
@@ -1,156 +1,156 @@
-// import 'package:flutter/material.dart';
-// import 'package:stacked/stacked.dart';
-// import '../problem_screen/problem_screen_vu.dart';
-// import 'item_screen_vm.dart';
+import 'package:flutter/material.dart';
+import 'package:stacked/stacked.dart';
+import 'item_screen_vm.dart';
 
-// class ItemScreenVU extends StackedView<ItemScreenVM> {
-//   const ItemScreenVU({super.key});
+class ItemScreenVU extends StackedView<ItemScreenVM> {
+  const ItemScreenVU({super.key});
 
-//   @override
-//   Widget builder(BuildContext context, ItemScreenVM viewModel, Widget? child) {
-//     final screenSize = MediaQuery.of(context).size;
-//     return Scaffold(
-//         body: Ink(
-//       color: const Color.fromARGB(255, 238, 238, 238),
-//       child: Column(
-//         crossAxisAlignment: CrossAxisAlignment.center,
-//         children: [
-//           const Header(),
-//           const SizedBox(
-//             height: 50,
-//           ),
-//           Expanded(
-//             child: Padding(
-//               padding: const EdgeInsets.symmetric(horizontal: 40),
-//               child: SingleChildScrollView(
-//                 child: Column(
-//                   children: [
-//                     const ImageContainer(),
-//                     const SizedBox(height: 20),
-//                     GenericAnswers(
-//                         viewModel: viewModel, screenSize: screenSize),
-//                   ],
-//                 ),
-//               ),
-//             ),
-//           ),
-//         ],
-//       ),
-//     ));
-//   }
+  @override
+  Widget builder(BuildContext context, ItemScreenVM viewModel, Widget? child) {
+    final screenSize = MediaQuery.of(context).size;
+    return Scaffold(
+        body: Ink(
+      color: const Color.fromARGB(255, 238, 238, 238),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const Header(),
+          const SizedBox(
+            height: 50,
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 40),
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    const ImageContainer(),
+                    const SizedBox(height: 20),
+                    GenericAnswers(
+                        viewModel: viewModel, screenSize: screenSize),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    ));
+  }
 
-//   @override
-//   ItemScreenVM viewModelBuilder(BuildContext context) => ItemScreenVM();
-// }
+  @override
+  ItemScreenVM viewModelBuilder(BuildContext context) => ItemScreenVM();
+}
 
-// class GenericAnswers extends StatelessWidget {
-//   final ItemScreenVM viewModel;
-//   final Size screenSize;
-//   const GenericAnswers(
-//       {super.key, required this.viewModel, required this.screenSize});
+class GenericAnswers extends StatelessWidget {
+  final ItemScreenVM viewModel;
+  final Size screenSize;
+  const GenericAnswers(
+      {super.key, required this.viewModel, required this.screenSize});
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return ConstrainedBox(
-//       constraints: const BoxConstraints(maxWidth: 450, maxHeight: 500),
-//       child: ListView.builder(
-//         // physics: const NeverScrollableScrollPhysics(),
-//         itemCount: viewModel.testList.length,
-//         itemBuilder: (context, index) {
-//           return Container(
-//               margin: const EdgeInsets.only(bottom: 10),
-//               padding: const EdgeInsets.all(24),
-//               decoration: BoxDecoration(
-//                 color: Colors.white,
-//                 borderRadius: BorderRadius.circular(10),
-//               ),
-//               child: Row(
-//                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//                 children: [
-//                   SizedBox(
-//                     width: screenSize.width > 600 ? 350 : 230,
-//                     child: Text(
-//                       maxLines: 3,
-//                       overflow: TextOverflow.ellipsis,
-//                       viewModel.testList[index],
-//                       style: const TextStyle(fontWeight: FontWeight.bold),
-//                     ),
-//                   ),
-//                   IconButton(
-//                       onPressed: () {
-//                          Navigator.push(
-//                               context,
-//                               MaterialPageRoute(
-//                                   builder: (context) => const ProblemScreenVU()));
-//                       },
-//                       icon: const Icon(Icons.keyboard_arrow_right_outlined))
-//                 ],
-//               ));
-//         },
-//       ),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return ConstrainedBox(
+      constraints: const BoxConstraints(maxWidth: 450, maxHeight: 500),
+      child: ListView.builder(
+        // physics: const NeverScrollableScrollPhysics(),
+        itemCount: viewModel.testList.length,
+        itemBuilder: (context, index) {
+          return Container(
+              margin: const EdgeInsets.only(bottom: 10),
+              padding: const EdgeInsets.all(24),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SizedBox(
+                    width: screenSize.width > 600 ? 350 : 230,
+                    child: Text(
+                      maxLines: 3,
+                      overflow: TextOverflow.ellipsis,
+                      viewModel.testList[index],
+                      style: const TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  IconButton(
+                      onPressed: () {
+                        //  Navigator.push(
+                        //       context,
+                        //       MaterialPageRoute(
+                        //           builder: (context) => const ProblemScreenVU())
+                        //           );
+                      },
+                      icon: const Icon(Icons.keyboard_arrow_right_outlined))
+                ],
+              ));
+        },
+      ),
+    );
+  }
+}
 
-// class ImageContainer extends StatelessWidget {
-//   const ImageContainer({
-//     super.key,
-//   });
+class ImageContainer extends StatelessWidget {
+  const ImageContainer({
+    super.key,
+  });
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
-//       decoration: BoxDecoration(
-//         color: Colors.white,
-//         borderRadius: BorderRadius.circular(10),
-//       ),
-//       height: 300,
-//       width: 450,
-//       child: const Image(image: AssetImage('lib/assets/images/compressor.png')),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      height: 300,
+      width: 450,
+      child: const Image(image: AssetImage('lib/assets/images/compressor.png')),
+    );
+  }
+}
 
-// class Header extends StatelessWidget {
-//   const Header({
-//     super.key,
-//   });
+class Header extends StatelessWidget {
+  const Header({
+    super.key,
+  });
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
-//       decoration: BoxDecoration(
-//         boxShadow: [
-//           BoxShadow(
-//             color: Colors.grey.withOpacity(0.5),
-//             spreadRadius: 0,
-//             blurRadius: 7,
-//             offset: const Offset(0, 3), // changes position of shadow
-//           ),
-//         ],
-//         color: const Color.fromARGB(255, 238, 238, 238),
-//       ),
-//       height: 100,
-//       child: Row(
-//         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//         children: [
-//           IconButton(
-//               onPressed: () {
-//                 Navigator.pop(context);
-//               },
-//               icon: const Icon(size: 50, Icons.keyboard_arrow_left)),
-//           const Text(
-//             "Problem",
-//             textAlign: TextAlign.left,
-//             style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-//           ),
-//           const Icon(
-//             size: 50,
-//             Icons.keyboard_arrow_left,
-//             color: Color.fromARGB(0, 255, 214, 64),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 0,
+            blurRadius: 7,
+            offset: const Offset(0, 3), // changes position of shadow
+          ),
+        ],
+        color: const Color.fromARGB(255, 238, 238, 238),
+      ),
+      height: 100,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          IconButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: const Icon(size: 50, Icons.keyboard_arrow_left)),
+          const Text(
+            "Problem",
+            textAlign: TextAlign.left,
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+          const Icon(
+            size: 50,
+            Icons.keyboard_arrow_left,
+            color: Color.fromARGB(0, 255, 214, 64),
+          ),
+        ],
+      ),
+    );
+  }
+}
